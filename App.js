c7aacef097e99fe80f3b683f0718f092638b518c
sophiesoe <devsophiesoe@gmail.com>
2025-07-16 15:05:25 +0630

    add: volume manager app

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { View, StyleSheet, Button } from "react-native";
-import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
+import { useEffect, useRef, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+  Button,
+  Alert,
+  SafeAreaView,
+} from "react-native";
+import {
+  VolumeManager,
+  useRingerMode,
+  RINGER_MODE,
+} from "react-native-volume-manager";
 import Slider from "@react-native-community/slider";
-import { useEffect } from "react";
+import { useAudioPlayer, useAudioPlayerStatus } from "expo-audio";
 
 const audioSource = require("./assets/Hello.mp3");
 
-export default function Audio() {
+const modeText = {
+  [RINGER_MODE.silent]: "Silent",
+  [RINGER_MODE.normal]: "Normal",
+  [RINGER_MODE.vibrate]: "Vibrate",
+};
+
+function nullishBooleanToString(value) {
+  return value === undefined ? "unset" : value ? "YES" : "NO";
+}
+
+export default function VolumeManagerApp() {
+  const [currentSystemVolume, setReportedSystemVolume] = useState(0);
+  const [isMuted, setIsMuted] = useState();
+  const [initialQuery, setInitialQuery] = useState();
+  const [ringerStatus, setRingerStatus] = useState();
+  const [hideUI, setHideUI] = useState(false);
+  const volumeChangedByListener = useRef(true);
+
   const player = useAudioPlayer(audioSource);
   const status = useAudioPlayerStatus(player);
 
@@ -19,58 +51,254 @@ export default function Audio() {
   };
 
   useEffect(() => {
-    // console.log("player =>", player);
-    // console.log("player status =>", status);
-  }, [player]);
+    VolumeManager.showNativeVolumeUI({ enabled: !hideUI });
+  }, [hideUI]);
+
+  useEffect(() => {
+    // Enable or disable iOS AudioSession
+    VolumeManager.enable(true, true); // Enable async
+    VolumeManager.enable(false, true); // Disable async, non-blocking
+
+    // Activate or deactivate the audio session
+    VolumeManager.setActive(true, true); // Activate async
+    VolumeManager.setActive(false, true); // Deactivate async, non-blocking
+
+    VolumeManager.getVolume().then((result) => {
+      setReportedSystemVolume(result.volume);
+      console.log("Read system volume", result);
+    });
+
+    const volumeListener = VolumeManager.addVolumeListener((result) => {
+      volumeChangedByListener.current = true;
+
+      if (
+        (Platform.OS === "android" && result.type === "music") ||
+        Platform.OS === "ios"
+      ) {
+        setReportedSystemVolume(result.volume);
+        console.log("Volume changed, updating state", result);
+      } else {
+        console.log(
+          "Volume changed, but not for type music, not updating state",
+          result
+        );
+      }
+    });
+
+    const silentListener = VolumeManager.addSilentListener((status) => {
+      console.log(status);
+      setIsMuted(status.isMuted);
+      setInitialQuery(status.initialQuery);
+    });
+
+    const ringerListener = VolumeManager.addRingerListener((result) => {
+      console.log("Ringer listener changed", result);
+      setRingerStatus(result);
+    });
+
+    return () => {
+      volumeListener.remove();
+      silentListener.remove();
+      VolumeManager.removeRingerListener(ringerListener);
+    };
+  }, []);
+
+  const { mode, error, setMode } = useRingerMode();
 
   return (
-    <View style={styles.container}>
-      <Slider
-        style={styles.slider}
-        minimumValue={0}
-        maximumValue={player.duration}
-        value={player.currentTime}
-        onSlidingComplete={onSlide}
-        tapToSeek
-      />
-      <View style={styles.controls}>
-        <Button title="Play" onPress={() => player.play()} />
-        <Button title="Pause" onPress={() => player.pause()} />
-        <Button title="Replay" onPress={replaySoundHandler} />
-      </View>
-    </View>
+    <SafeAreaView style={{ flex: 1, padding: 20 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        {!isNaN(player.duration) && player.duration > 0 ? (
+          <View style={styles.container}>
+            <Slider
+              style={styles.slider}
+              minimumValue={0}
+              maximumValue={player.duration}
+              value={player.currentTime}
+              onSlidingComplete={onSlide}
+              tapToSeek
+            />
+            <View style={styles.controls}>
+              <Button title="Play" onPress={() => player.play()} />
+              <Button title="Pause" onPress={() => player.pause()} />
+              <Button title="Replay" onPress={replaySoundHandler} />
+            </View>
+          </View>
+        ) : (
+          ""
+        )}
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>iOS / Android</Text>
+          <View style={styles.row}>
+            <Text style={styles.text}>Current volume:</Text>
+            <Text style={styles.text}>{currentSystemVolume}</Text>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.text}>Is muted?:</Text>
+            <Text style={styles.text}>
+              {currentSystemVolume <= 0 ? "YES" : "NO"}
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 10 }}>
+            <Text style={styles.text}>
+              Volume update {hideUI ? "(without toast)" : "(with toast)"}
+            </Text>
+          </View>
+          <Slider
+            style={{ width: "100%", height: 40 }}
+            minimumValue={0}
+            maximumValue={1}
+            minimumTrackTintColor="#000"
+            maximumTrackTintColor="#999"
+            onValueChange={(value) => {
+              VolumeManager.setVolume(value, { showUI: !hideUI });
+            }}
+            onSlidingComplete={async (value) => {
+              setReportedSystemVolume(value);
+            }}
+            value={currentSystemVolume}
+            step={0.001}
+          />
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setHideUI((shouldHide) => !shouldHide)}
+          >
+            <Text style={styles.buttonText}>
+              {hideUI ? "Show native volume Toast" : "Hide native volume Toast"}
+            </Text>
+          </TouchableOpacity>
+          <View style={{ height: 10 }} />
+
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>iOS only features</Text>
+            <View style={styles.row}>
+              <Text style={styles.text}>Silent switch active?:</Text>
+              <Text style={styles.text}>
+                {Platform.OS === "ios"
+                  ? `${nullishBooleanToString(
+                      isMuted
+                    )} (initial query: ${nullishBooleanToString(initialQuery)})`
+                  : "Unsupported on Android"}
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.card}>
+            <Text style={styles.cardTitle}>Android only features</Text>
+            <View style={styles.row}>
+              <Text style={styles.text}>Ringer Mode listener:</Text>
+              <Text style={styles.text}>{ringerStatus?.mode}</Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.text}>Selected Ringer Mode:</Text>
+              <Text style={styles.text}>
+                {mode !== undefined
+                  ? modeText[mode]
+                  : Platform.OS === "ios"
+                  ? "Unsupported on iOS"
+                  : "Unknown"}
+              </Text>
+            </View>
+
+            <View style={{ marginTop: 20 }}>
+              <Text style={styles.text}>Set Ringer mode:</Text>
+              <View
+                style={{
+                  marginTop: 20,
+                  height: 120,
+                  justifyContent: "space-between",
+                  gap: 10,
+                }}
+              >
+                <Button
+                  title="Silent"
+                  onPress={() => setMode(RINGER_MODE.silent)}
+                />
+                <Button
+                  title="Normal"
+                  onPress={() => setMode(RINGER_MODE.normal)}
+                />
+                <Button
+                  title="Vibrate"
+                  onPress={() => setMode(RINGER_MODE.vibrate)}
+                />
+                <Button
+                  title="Check if device is silent"
+                  onPress={async () => {
+                    const result = await VolumeManager.isAndroidDeviceSilent();
+                    Alert.alert(
+                      "Info",
+                      result
+                        ? "Device is silent. This is a silent mode or muted volume or vibrate mode or do not disturb mode."
+                        : "Device is not silent. This is a normal mode. Check ringer mode to get more details"
+                    );
+                  }}
+                />
+              </View>
+            </View>
+
+            <View>
+              <View />
+              <View
+                style={{
+                  marginTop: 30,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text style={styles.text}>{error?.message}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
+    padding: 20,
+    backgroundColor: "#F5F5F5",
+  },
+  card: {
     padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "#DDD",
+    backgroundColor: "#FFF",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "600",
-    textAlign: "center",
-    marginBottom: 20,
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
-  slider: {
-    width: "100%",
-    height: 50,
-  },
-  timeContainer: {
+  row: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginVertical: 8,
+    marginBottom: 10,
+    flexWrap: "wrap",
   },
-  time: {
-    fontSize: 14,
-    color: "#555",
+  text: {
+    fontSize: 16,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#000",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#FFF",
+    textAlign: "center",
   },
   controls: {
     flexDirection: "row",
     justifyContent: "center",
-    rowGap: 20,
+    gap: 20,
   },
 });
---
+++ READ_ME.md
@@ -0,0 +1,15 @@
+# React Native Volume Manager With Expo 53 Latest
+
+## For Android
+
+The volume control works both **Emulators and Real Devices**.
+
+## For iOS
+
+The volume control should test only on **Real Devices**.  
+In **Simulators**, the behavior will not work.
+
+### Quick Solution
+
+If there any build errors in Android Native UI,  
+Please remove `package.lock.json` file and `nodule_modules` folder before running `npx expo prebuild` and `npx expo run:android`
